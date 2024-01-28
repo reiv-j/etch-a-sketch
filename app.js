f75958997ce4e129e6c54efd95289f40b08d110d
reiv <javierromaynieto@gmail.com>
2024-01-28 16:51:31 +0100

    Create size button and createGrid function

---
+++ app.js
@@ -0,0 +1,16 @@
+const boxesContainer = document.querySelector(".boxes-container");
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    const size = prompt("Please choose the size of the grid (4 - 32): ");
+    createGrid(Number(size));
+})
+
+function createGrid(size) {
+    for (let i = 0; i < size*size; i++) {
+        const newBox = document.createElement("div");
+        newBox.classList.add("box");
+        boxesContainer.appendChild(newBox);
+    }
+}
+
